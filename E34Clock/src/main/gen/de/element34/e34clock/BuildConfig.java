d4b762671499626a9cc467cdbd277a32e7ba0c4b
Stefan Langer <mailtolanger@googlemail.com>
2013-10-18 09:15:13 +0200

    Initial commit of empty project
    Rudimentary layouts added
    Generated classes from android studio added
    Generated project from android studio added

---
+++ E34Clock/src/main/gen/de/element34/e34clock/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package de.element34.e34clock;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
