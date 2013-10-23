7da5ecf708843f323d59ce0cda5c2aeb56d1267f
Stefan Langer <mailtolanger@googlemail.com>
2013-10-23 07:57:30 +0200

    Added copyright information

--- E34Clock/src/main/java/de/element34/e34clock/ClockWidget.java
+++ E34Clock/src/main/java/de/element34/e34clock/ClockWidget.java
@@ -1,3 +1,19 @@
+/*
+ * (c) Copyright 2013 Stefan Langer
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package de.element34.e34clock;
 
 import android.appwidget.AppWidgetManager;
@@ -13,10 +29,6 @@ import static de.element34.e34clock.Constants.BATTERY_ACTION;
 import static de.element34.e34clock.Constants.BATTERY_IMG;
 import static de.element34.e34clock.Constants.BATTERY_LEVEL;
 
-/**
- * Copyrigfht 2013
- * Created by slanger on 18.10.13.
- */
 public class ClockWidget extends AppWidgetProvider {
     private static String TAG = "e34clock.ClockWidget";
 
--- E34Clock/src/main/java/de/element34/e34clock/Constants.java
+++ E34Clock/src/main/java/de/element34/e34clock/Constants.java
@@ -1,9 +1,21 @@
+/*
+ * (c) Copyright 2013 Stefan Langer
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package de.element34.e34clock;
 
-/**
- * Copyrigfht 2013
- * Created by slanger on 18.10.13.
- */
 public class Constants {
     public static final String BATTERY_ACTION = "de.element34.e34clock.action.BATTERY";
     public static final String BATTERY_LEVEL = "BATTERY_LEVEL";
